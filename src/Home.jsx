d019bd189365b539570c13a669cda6b2210aec35
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-06 15:47:44 +0530

    making changes and adding a branch

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,40 +3,26 @@ import { useState } from "react";
 import BlogList from "./Bloglist";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  const [name, setName] = useState("nidhin")
-
-  const handleDelete = (id) => {
-    const newblogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newblogs);
-  };
-
-   useEffect(() => {
-     fetch("http://localhost:8000/blogs")
-       .then((res) => {
-         return res.json();
-       })
-       .then((data) => {
-         setBlogs(data);
-       });
-   }, []);
-  
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setIsPending(false);
+          setBlogs(data);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home">
-     <BlogList blogs={blogs} title= "All Blogs"  handleDelete={handleDelete}/>
-     <button onClick={()=> setName("Nikhil")}>change name {name}</button>
-     {/* <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title = "Marios blog"/> */}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
