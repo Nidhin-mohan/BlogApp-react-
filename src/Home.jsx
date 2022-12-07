aa5e78e5df4ec10e2c54ed8eca004299fa5c821a
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-07 20:01:26 +0530

    costom hooks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,14 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./Bloglist";
 
-const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-   const [error, setError] = useState(null);
+import useFetch from "./useFetch";
 
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("  https://api.npoint.io/b4ead184047c83f15b75/blogs")
-        .then((res) => {
-           if (!res.ok) {
-             // error coming back from server
-             throw Error("could not fetch the data for that resource");
-           } 
-          return res.json();
-        })
-        .then((data) => {
-          setError(null);
-          setIsPending(false);
-          setBlogs(data);
-        })
-        .catch((err) => {
-          // auto catches network / connection error
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+const Home = () => {
+  const {
+    error,
+    isPending,
+    data: blogs,
+  } = useFetch("https://api.npoint.io/b4ead184047c83f15b75/blogs");
 
   return (
     <div className="home">
