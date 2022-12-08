21ea40536b2ed12ba12a50dc8e3c66dde29b341f
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-08 17:05:32 +0530

    Added feature -delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
+
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+
+    const handleClick = () => {
+      fetch("http://localhost:8000/blogs/" + blog.id, {
+        method: "DELETE",
+      }).then(() => {
+         navigate("/");
+      });
+    };
 
   return (
     <div className="blog-details">
@@ -18,6 +30,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
